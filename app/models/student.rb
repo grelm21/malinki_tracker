12ee77babe682d36ea502f096d466fef0c1fec47
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-08-15 19:47:27 +0300

    fixed payment type and creation of student

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,19 +3,21 @@ class Student < ApplicationRecord
   has_many :classrooms_students
   has_many :classrooms, through: :classrooms_students
 
-  attribute :classroom_id, default: nil
+  attribute :classroom_id, type: :string, default: nil
   attribute :wage, default: 0
   attribute :length, default: 0
-  attribute :payment_type, default: 'single'
+  attribute :payment_type, default: 'regular'
   attribute :schedule, default: []
 
   validates :name, presence: true
 
-  after_create_commit :create_classroom, if: -> { classroom_id.blank? }
+  after_create_commit :create_classroom, if: -> { classroom_id.eql?('') }
 
   private
+
   def create_classroom
-    classroom = Classroom.create(name:, wage:, length:, schedule:, user: user)
-    ClassroomsStudent.create(student: self, classroom:, payment_type:)
+    p '_---___---___---___---'
+    classroom = Classroom.create!(name:, wage:, length:, schedule:, user: user)
+    ClassroomsStudent.create!(student: self, classroom:, payment_type:)
   end
 end
