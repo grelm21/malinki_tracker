0a76452919ee309a009e9ee2032ea99803a064c8
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-08-25 13:58:57 +0300

    added color vars in css file view refactor started

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   content: [
-    './app/views/**/*.{erb,html}',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.{js,jsx,ts,tsx}',
+    "./app/views/**/*.{erb,html}",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
